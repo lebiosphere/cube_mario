bf73705e5f89011cdeb433c1f296f796cae2bbde
lebiosphere <lebiosphere@gmail.com>
2023-06-28 17:49:29 +0200

    derniere adresse

--- main.ts
+++ main.ts
@@ -1,35 +1,35 @@
 radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == adresse) {
         lumiere = 1
-        strip.showColor(neopixel.colors(NeoPixelColors.White))
+        strip.showColor(neopixel.colors(NeoPixelColors.Yellow))
     }
 })
 input.onButtonPressed(Button.A, function () {
+    led.unplot(adresse, 0)
+    led.unplot(suivant, 1)
     adresse += 1
     if (adresse > maxAdresse) {
         adresse = 0
     }
     suivant = adresse + 1
-    if (suivant > 9) {
+    if (suivant > maxAdresse) {
         suivant = 0
-        basic.pause(100)
     }
-    basic.clearScreen()
-    led.plot(Math.min(adresse, 4), Math.floor(adresse / 5))
+    basic.pause(200)
 })
 input.onButtonPressed(Button.B, function () {
     if (dernier == 0) {
         dernier = 1
         led.plot(4, 4)
         lumiere = 1
-        strip.showColor(neopixel.colors(NeoPixelColors.White))
+        strip.showColor(neopixel.colors(NeoPixelColors.Yellow))
     } else {
         dernier = 0
         led.unplot(4, 4)
         lumiere = 0
         strip.showColor(neopixel.hsl(255, 125, 10))
     }
-    basic.pause(100)
+    basic.pause(200)
 })
 input.onGesture(Gesture.Shake, function () {
     if (lumiere == 1) {
@@ -46,9 +46,24 @@ let maxAdresse = 0
 let strip: neopixel.Strip = null
 basic.clearScreen()
 radio.setGroup(35)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    # # # # #
+    # # # # #
+    . # # # .
+    `)
 strip = neopixel.create(DigitalPin.P0, 30, NeoPixelMode.RGB)
 maxAdresse = 4
 adresse = 0
 suivant = 1
 dernier = 0
 lumiere = 0
+strip.showColor(neopixel.hsl(255, 125, 10))
+basic.forever(function () {
+    led.plot(adresse, 0)
+    led.plot(suivant, 1)
+    if (dernier == 1) {
+        suivant = 0
+    }
+})
